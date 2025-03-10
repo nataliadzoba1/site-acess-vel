f01957d28fc2f712873588f6b16ce1429f2ef27c
natalia dzoba <145125394+nataliadzoba1@users.noreply.github.com>
2025-03-10 09:18:50 -0300

    tamanho fonte

--- iscript.js
+++ iscript.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminui-fonte');
+    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 });
